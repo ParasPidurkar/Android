8cc8424abb76c99ce91cf4edc560fe1c6d9e9fc9
Paras <paraspidurkar97@gmail.com>
2024-07-17 01:44:25 +0530

    Add files via upload

---
+++ L01_Basics/10_Strings.kt
@@ -0,0 +1,26 @@
+package L01_Basics
+
+fun main() {
+    val myvalue : String = "ABC"
+    println(myvalue)
+
+    val firstname = "paras"
+    val lastname = "pidurkar"
+    println("The first value is $firstname and the second value is $lastname")
+
+
+    //predefined functions
+
+    //a) length
+    println(firstname.length)
+
+    //b) capitalize
+    println(firstname.capitalize())
+    println(firstname)
+
+    //b) toUpperCase
+    println(firstname.toUpperCase())
+
+    //b) toLowerCase
+    println(firstname.toLowerCase())
+}
